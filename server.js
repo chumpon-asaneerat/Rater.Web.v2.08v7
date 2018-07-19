cf4a8febc3ef9f2f3f1f5ea913a602501003e0ba
chumpon-asaneerat <chumponsenate@gmail.com>
2018-07-19 13:50:54 +0700

    Add package.json and typings.json and install node_modules

---
+++ server.js
@@ -0,0 +1,5 @@
+﻿const path = require('path');
+// setup root path.
+process.env['ROOT_PATHS'] = path.dirname(require.main.filename);
+const rootPath = process.env['ROOT_PATHS'];
+const libPath = path.join(rootPath, 'lib');
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express-handlebars/index.d.ts" />
+/// <reference path="globals/express-session/index.d.ts" />
+/// <reference path="globals/pug/index.d.ts" />
+/// <reference path="globals/riot/index.d.ts" />
