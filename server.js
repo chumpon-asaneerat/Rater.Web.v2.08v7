ef41e1f3b25ea80e2a2cd7a00b6176d2aeb94d6d
chumpon-asaneerat <chumponsenate@gmail.com>
2018-07-31 07:22:05 +0700

    Update server.js from v2.08v6 v2

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-﻿const path = require('path');
+﻿const express = require('express');
+const path = require('path');
+
 // setup root path.
 process.env['ROOT_PATHS'] = path.dirname(require.main.filename);
-const rootPath = process.env['ROOT_PATHS'];
-const libPath = path.join(rootPath, 'lib');
+//console.log('root path: ', process.env['ROOT_PATHS']);
+
+const conf = require('./configs/app-configs');
+const middleware = require('./lib/app-middlewares');
+const commonpaths = require('./lib/app-paths');
+const routes = require('./lib/app-routes');
+const nlib = require('./lib/nlib-core');
+
+// create express middle ware instance.
+var app = express();
+// setup port
+app.set('port', process.env.PORT || conf.server.portNumber);
+// setup middlewares.
+middleware.init(app);
+// setip common path(s).
+commonpaths.init(app);
+// setup routes
+routes.init(app);
+
+// start server.
+var server = app.listen(app.get('port'), function () {
+    var appName = conf.app.name + ' ' + conf.app.version;
+    var portName = server.address().port;
+    console.log(appName + ' listening on port ' + portName);
+});
